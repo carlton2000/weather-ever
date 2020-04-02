53506bc5d204c5ee018be73be6953bbab3feae90
Carlton Visagie <53076687+carlton2000@users.noreply.github.com>
2020-04-02 17:07:17 +0200

    Update App.js
    changed the fetch api key from http - https

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     //Api call below, we put in the city and country and provide the hard coded unique API key
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${theKEY}&units=metric`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${theKEY}&units=metric`);
     //JSON file returned and read
     // This variable fetches the api info from the map that was given  
     const data = await api_call.json();
